132843a9393c32cbd0ee8ff14f2e014cfb38d46e
Greyson Nevins <greyman1995@gmail.com>
2015-05-17 00:43:44 -0400

    Changed file to work with splash page

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+MAIN_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'coffeed.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(MAIN_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,3 +100,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+ os.path.join(MAIN_DIR, 'static'),
+ )
+
+STATIC_ROOT = 'staticfiles'
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+    )
+
+     
--- coffeed/urls.py
+++ coffeed/urls.py
@@ -4,9 +4,8 @@ import core.views as coreviews
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'coffeed.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    #url(r'^$', 'coffeed.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', coreviews.TestView),
+    url(r'^$', coreviews.SplashView.as_view()),
+
 )
